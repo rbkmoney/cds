4298d417e896b58848e0aebc855a6ca9d55216a5
Artem Ocheredko <galaxie.stern@gmail.com>
2016-10-14 19:21:44 +0300

    CDS-17 Added keyring file storage again (#10)
    * CDS-17 Added keyring file storage

---
+++ apps/cds/src/cds_keyring_storage_file.erl
@@ -0,0 +1,54 @@
+-module(cds_keyring_storage_file).
+-behaviour(cds_keyring_storage).
+
+-export([create/1]).
+-export([read/0]).
+-export([update/1]).
+-export([delete/0]).
+
+-define(DEFAULT_KEYRING_PATH, "/var/cds/keyring").
+
+-spec create(binary()) -> ok | {error, already_exists}.
+create(Keyring) ->
+    Path = application:get_env(cds, keyring_path, ?DEFAULT_KEYRING_PATH),
+    case filelib:is_regular(Path) of
+        false ->
+            ok = filelib:ensure_dir(Path),
+            ok = atomic_write(Path, Keyring);
+        true ->
+            {error, already_exists}
+    end.
+
+-spec read() -> {ok, binary()} | {error, not_found}.
+read() ->
+    Path = application:get_env(cds, keyring_path, ?DEFAULT_KEYRING_PATH),
+    case file:read_file(Path) of
+        {ok, Data} ->
+            {ok, Data};
+        {error, enoent} ->
+            {error, not_found}
+    end.
+
+-spec update(binary()) -> ok.
+update(Keyring) ->
+    Path = application:get_env(cds, keyring_path, ?DEFAULT_KEYRING_PATH),
+    ok = filelib:ensure_dir(Path),
+    ok = atomic_write(Path, Keyring).
+
+-spec delete() -> ok.
+delete() ->
+    Path = application:get_env(cds, keyring_path, ?DEFAULT_KEYRING_PATH),
+    case file:delete(Path) of
+        ok ->
+            ok;
+        {error, enoent} ->
+            ok
+    end.
+
+atomic_write(Path, Keyring) ->
+    TmpPath = tmp_keyring_path(Path),
+    ok = file:write_file(TmpPath, Keyring),
+    file:rename(TmpPath, Path).
+
+tmp_keyring_path(Path) ->
+    genlib:to_list(Path) ++ ".tmp".
---
+++ apps/cds/test/cds_keyring_storage_tests_SUITE.erl
@@ -0,0 +1,68 @@
+-module(cds_keyring_storage_tests_SUITE).
+-include_lib("common_test/include/ct.hrl").
+-compile(export_all).
+
+all() ->
+    [
+        {group, env_storage_lifecycle},
+        {group, file_storage_lifecycle}
+    ].
+
+groups() ->
+    [
+        {env_storage_lifecycle, [sequence],[
+            create,
+            already_exists,
+            read,
+            update,
+            delete
+        ]},
+        {file_storage_lifecycle, [sequence],[
+            create,
+            already_exists,
+            read,
+            update,
+            delete
+        ]}
+    ].
+
+%%
+%% starting/stopping
+%%
+
+init_per_group(env_storage_lifecycle, C) ->
+    application:set_env(cds, keyring_storage, cds_keyring_storage_env),
+    C;
+
+init_per_group(file_storage_lifecycle, C) ->
+    application:set_env(cds, keyring_storage, cds_keyring_storage_file),
+    application:set_env(cds, keyring_path, keyring_storage_file_path(C)),
+    C;
+
+init_per_group(_, C) ->
+    C.
+
+end_per_group(_, _C) ->
+    ok.
+
+create(_C) ->
+    Keyring = <<"initial">>,
+    ok = cds_keyring_storage:create(Keyring).
+
+already_exists(_C) ->
+    Keyring = <<"bla">>,
+    already_exists = (catch cds_keyring_storage:create(Keyring)).
+
+read(_C) ->
+    <<"initial">> = cds_keyring_storage:read().
+
+update(_C) ->
+    NewKeyring = <<"updated keyring">>,
+    cds_keyring_storage:update(NewKeyring),
+    NewKeyring = cds_keyring_storage:read().
+
+delete(_C) ->
+    ok = cds_keyring_storage:delete().
+
+keyring_storage_file_path(C) ->
+    filename:join([?config(priv_dir, C), "keyring"]).
