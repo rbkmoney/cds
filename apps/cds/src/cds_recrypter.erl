936f815a7dbccff6ebc50de5f84caa72b4908764
Evgeny Levenets <levenets.e.e@gmail.com>
2017-06-15 16:39:54 +0300

    CDS-21: fixed recrypter race
    * fixed race, when recrypter starts faster then keyring

--- apps/cds/src/cds_recrypter.erl
+++ apps/cds/src/cds_recrypter.erl
@@ -57,8 +57,8 @@ process_recrypting(EncodingType, BatchSize, Continuation) ->
         Intervals = cds_keyring_manager:get_outdated_keys(),
         process_recrypting(EncodingType, Intervals, BatchSize, Continuation)
     catch
-        throw:locked ->
-            {error, locked}
+        throw:Reason ->
+            {error, Reason}
     end.
 
 process_recrypting(_, [], _, _) ->
