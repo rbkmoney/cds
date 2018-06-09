627210a80684d5571cc6c8795da54dc043b920e3
Andrey Fadeev <me@ciiol.net>
2018-06-09 16:23:32 +0300

    HG-364 Add simple tests for identity documents storage

--- apps/cds/test/cds_api_tests_SUITE.erl
+++ apps/cds/test/cds_api_tests_SUITE.erl
@@ -134,26 +134,10 @@ groups() ->
 -spec init_per_group(atom(), config()) -> config().
 
 init_per_group(riak_storage_backend, C) ->
-    % _ = dbg:tracer(),
-    % _ = dbg:p(all, c),
-    % _ = dbg:tpl({cds_storage_riak, '_', '_'}, x),
-    StorageConfig = [
-        {storage, cds_storage_riak},
-        {cds_storage_riak, #{
-            conn_params => #{
-                host => "riakdb",
-                port => 8087
-            },
-            timeout => 5000
-        }}
-    ],
-    [{storage_config, StorageConfig} | C];
+    cds_ct_utils:set_riak_storage(C);
 
 init_per_group(ets_storage_backend, C) ->
-    StorageConfig = [
-        {storage, cds_storage_ets}
-    ],
-    [{storage_config, StorageConfig} | C];
+    cds_ct_utils:set_etc_storage(C);
 
 init_per_group(general_flow, C) ->
     C;
@@ -162,7 +146,7 @@ init_per_group(keyring_errors, C) ->
     StorageConfig = [
         {storage, cds_storage_ets}
     ],
-    C1 = start_clear([{storage_config, StorageConfig} | C]),
+    C1 = cds_ct_utils:start_clear([{storage_config, StorageConfig} | C]),
     C1 ++ C;
 
 init_per_group(session_management, C) ->
@@ -182,11 +166,11 @@ init_per_group(session_management, C) ->
         }}
     ],
     C1 = [{recrypting_config, Recrypting}, {session_cleaning_config, CleanerConfig} | C],
-    C2 = start_clear(C1),
+    C2 = cds_ct_utils:start_clear(C1),
     C1 ++ C2;
 
 init_per_group(_, C) ->
-    C1 = start_clear(C),
+    C1 = cds_ct_utils:start_clear(C),
     C1 ++ C.
 
 -spec end_per_group(atom(), config()) -> _.
@@ -199,7 +183,7 @@ end_per_group(Group, C) when
     C;
 
 end_per_group(_, C) ->
-    stop_clear(C).
+    cds_ct_utils:stop_clear(C).
 
 %%
 %% tests
@@ -210,7 +194,7 @@ end_per_group(_, C) ->
 init(C) ->
     MasterKeys = cds_client:init(2, 3, root_url(C)),
     3 = length(MasterKeys),
-    store(master_keys, MasterKeys, C).
+    cds_ct_utils:store(master_keys, MasterKeys, C).
 
 -spec lock(config()) -> _.
 
@@ -220,7 +204,7 @@ lock(C) ->
 -spec unlock(config()) -> _.
 
 unlock(C) ->
-    [MasterKey1, MasterKey2, _MasterKey3] = lookup(master_keys, C),
+    [MasterKey1, MasterKey2, _MasterKey3] = cds_ct_utils:lookup(master_keys, C),
     {more_keys_needed, 1} = cds_client:unlock(MasterKey1, root_url(C)),
     {unlocked, #'Unlocked'{}} = cds_client:unlock(MasterKey2, root_url(C)).
 
@@ -243,17 +227,17 @@ put_card_data(C) ->
         },
         session_id = Session
     } = cds_client:put_card_data(?CREDIT_CARD(undefined), ?SESSION_DATA(?CARD_SEC_CODE(?CVV)), root_url(C)),
-    store([{token, Token}, {session, Session}], C).
+    cds_ct_utils:store([{token, Token}, {session, Session}], C).
 
 -spec get_card_data(config()) -> _.
 
 get_card_data(C) ->
-    ?CREDIT_CARD(<<>>) = cds_client:get_card_data(lookup(token, C), root_url(C)).
+    ?CREDIT_CARD(<<>>) = cds_client:get_card_data(cds_ct_utils:lookup(token, C), root_url(C)).
 
 -spec get_session_data(config()) -> _.
 
 get_session_data(C) ->
-    ?SESSION_DATA(?CARD_SEC_CODE(?CVV)) = cds_client:get_session_data(lookup(session, C), root_url(C)).
+    ?SESSION_DATA(?CARD_SEC_CODE(?CVV)) = cds_client:get_session_data(cds_ct_utils:lookup(session, C), root_url(C)).
 
 -spec put_card_data_3ds(config()) -> _.
 
@@ -264,17 +248,17 @@ put_card_data_3ds(C) ->
         },
         session_id = Session
     } = cds_client:put_card_data(?CREDIT_CARD(undefined), ?SESSION_DATA(?AUTH_3DS), root_url(C)),
-    store([{token, Token}, {session, Session}], C).
+    cds_ct_utils:store([{token, Token}, {session, Session}], C).
 
 -spec get_card_data_3ds(config()) -> _.
 
 get_card_data_3ds(C) ->
-    ?CREDIT_CARD(<<>>) = cds_client:get_card_data(lookup(token, C), root_url(C)).
+    ?CREDIT_CARD(<<>>) = cds_client:get_card_data(cds_ct_utils:lookup(token, C), root_url(C)).
 
 -spec get_session_data_3ds(config()) -> _.
 
 get_session_data_3ds(C) ->
-    ?SESSION_DATA(?AUTH_3DS) = cds_client:get_session_data(lookup(session, C), root_url(C)).
+    ?SESSION_DATA(?AUTH_3DS) = cds_client:get_session_data(cds_ct_utils:lookup(session, C), root_url(C)).
 
 -spec put_card_data_backward_compatibilty(config()) -> _.
 
@@ -285,22 +269,26 @@ put_card_data_backward_compatibilty(C) ->
         },
         session_id = Session
     } = cds_client:put_card_data(?CREDIT_CARD(?CVV), root_url(C)),
-    store([{token, Token}, {session, Session}], C).
+    cds_ct_utils:store([{token, Token}, {session, Session}], C).
 
 -spec get_card_data_backward_compatibilty(config()) -> _.
 
 get_card_data_backward_compatibilty(C) ->
-    ?CREDIT_CARD(<<>>) = cds_client:get_card_data(lookup(token, C), root_url(C)).
+    ?CREDIT_CARD(<<>>) = cds_client:get_card_data(cds_ct_utils:lookup(token, C), root_url(C)).
 
 -spec get_session_data_backward_compatibilty(config()) -> _.
 
 get_session_data_backward_compatibilty(C) ->
-    ?SESSION_DATA(?CARD_SEC_CODE(?CVV)) = cds_client:get_session_data(lookup(session, C), root_url(C)).
+    ?SESSION_DATA(?CARD_SEC_CODE(?CVV)) = cds_client:get_session_data(cds_ct_utils:lookup(session, C), root_url(C)).
 
 -spec get_session_card_data_backward_compatibilty(config()) -> _.
 
 get_session_card_data_backward_compatibilty(C) ->
-    ?CREDIT_CARD(?CVV) = cds_client:get_session_card_data(lookup(token, C), lookup(session, C), root_url(C)).
+    ?CREDIT_CARD(?CVV) = cds_client:get_session_card_data(
+        cds_ct_utils:lookup(token, C),
+        cds_ct_utils:lookup(session, C),
+        root_url(C)
+    ).
 
 -spec rotate(config()) -> _.
 
@@ -468,44 +456,6 @@ recrypt(C) ->
 %% helpers
 %%
 
-start_clear(Config) ->
-    IP = "127.0.0.1",
-    Port = 8022,
-    RootUrl = "http://" ++ IP ++ ":" ++ integer_to_list(Port),
-    StorageConfig = config(storage_config, Config, []),
-    CleanConfig = config(session_cleaning_config, Config, []),
-    Recrypting = config(recrypting_config, Config, []),
-    ok = clean_storage(StorageConfig),
-    Apps =
-        genlib_app:start_application_with(lager, [
-            {async_threshold, 1},
-            {async_threshold_window, 0},
-            {error_logger_hwm, 600},
-            {suppress_application_start_stop, true},
-            {crash_log, false},
-            {handlers, [
-                {lager_common_test_backend, warning}
-            ]}
-        ]) ++
-        genlib_app:start_application_with(cds, [
-            {ip, IP},
-            {port, Port},
-            {keyring_storage, cds_keyring_storage_env},
-            {net_opts, [
-                % Bump keepalive timeout up to a minute
-                {timeout, 60000}
-            ]}
-        ] ++ StorageConfig ++ CleanConfig ++ Recrypting)
-    ,
-    start_stash([
-        {apps, lists:reverse(Apps)},
-        {root_url, genlib:to_binary(RootUrl)}
-    ]).
-
-stop_clear(C) ->
-    _ = (catch cds_keyring_storage_env:delete()),
-    [ok = application:stop(App) || App <- config(apps, C)],
-    stop_stash(C).
 
 config(Key, Config) ->
     config(Key, Config, undefined).
@@ -521,48 +471,3 @@ config(Key, Config, Default) ->
 root_url(C) ->
     config(root_url, C).
 
-clean_storage(CdsEnv) ->
-    case genlib_opts:get(storage, CdsEnv) of
-        cds_storage_riak ->
-            clean_riak_storage(CdsEnv);
-        cds_storage_ets ->
-            ok
-    end.
-
-clean_riak_storage(CdsEnv) ->
-    _ = application:start(riakc),
-    _ = application:set_env(riakc, allow_listing, true),
-    #{conn_params := #{
-        host := Host,
-        port := Port
-    }} = genlib_opts:get(cds_storage_riak, CdsEnv),
-    {ok, Client} = riakc_pb_socket:start_link(Host, Port),
-    {ok, Buckets} = riakc_pb_socket:list_buckets(Client),
-    lists:foreach(
-        fun(B) ->
-            {ok, Keys} = riakc_pb_socket:list_keys(Client, B),
-            [
-                ok = riakc_pb_socket:delete(Client, B, K)
-                    || K <- Keys
-            ]
-        end,
-        Buckets
-    ),
-    ok.
-
-%%
-
-start_stash(C) ->
-    [{stash, cds_ct_stash:start()} | C].
-
-stop_stash(C) ->
-    cds_ct_stash:stop(config(stash, C)).
-
-store(KVs, C) when is_list(KVs) ->
-    [store(Key, Value, C) || {Key, Value} <- KVs].
-
-store(Key, Value, C) ->
-    cds_ct_stash:put(config(stash, C), Key, Value).
-
-lookup(Key, C) ->
-    cds_ct_stash:get(config(stash, C), Key).
---
+++ apps/cds/test/cds_identity_doc_tests_SUITE.erl
@@ -0,0 +1,148 @@
+-module(cds_identity_doc_tests_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("dmsl/include/dmsl_identity_document_storage_thrift.hrl").
+
+%% common_test callbacks
+-export([all/0]).
+-export([groups/0]).
+-export([init_per_group/2]).
+-export([end_per_group/2]).
+
+%% Tests
+
+-export([init/1]).
+-export([put_passport/1]).
+-export([put_insurance_cert/1]).
+-export([assert_doc_stored/1]).
+
+%%
+%% Internal types
+%%
+
+-type config() :: [{atom(), any()}] | atom().
+
+%%
+%% common_test callbacks
+%%
+
+-spec all() -> [{group, atom()}].
+all() ->
+    [
+        {group, riak_storage_backend},
+        {group, ets_storage_backend}
+    ].
+
+-spec groups() -> [{atom(), list(), [atom()]}].
+groups() ->
+    [
+        {riak_storage_backend, [], [{group, general_flow}]},
+        {ets_storage_backend, [], [{group, general_flow}]},
+        {general_flow, [], [
+            {group, basic_passport_lifecycle},
+            {group, basic_insurance_cert_lifecycle}
+        ]},
+        {basic_passport_lifecycle, [sequence], [
+            init,
+            put_passport,
+            assert_doc_stored
+        ]},
+        {basic_insurance_cert_lifecycle, [sequence], [
+            init,
+            put_insurance_cert,
+            assert_doc_stored
+        ]}
+    ].
+
+-spec init_per_group(atom(), config()) -> config().
+init_per_group(riak_storage_backend, C) ->
+    cds_ct_utils:set_riak_storage(C);
+init_per_group(ets_storage_backend, C) ->
+    cds_ct_utils:set_etc_storage(C);
+init_per_group(Group, C) when
+    Group =:= all;
+    Group =:= general_flow 
+->
+    C;
+init_per_group(_, C) ->
+    C1 = cds_ct_utils:start_clear(C),
+    C1 ++ C.
+
+-spec end_per_group(atom(), config()) -> _.
+end_per_group(Group, C) when
+    Group =:= all;
+    Group =:= ets_storage_backend;
+    Group =:= riak_storage_backend;
+    Group =:= general_flow
+ ->
+    C;
+end_per_group(_, C) ->
+    cds_ct_utils:stop_clear(C).
+
+%%
+%% Tests
+%%
+
+
+-spec init(config()) -> any() | no_return().
+init(C) ->
+    MasterKeys = cds_client:init(2, 3, root_url(C)),
+    3 = length(MasterKeys),
+    cds_ct_utils:store(master_keys, MasterKeys, C).
+
+-spec put_passport(config()) -> any() | no_return().
+put_passport(C) ->
+    Doc = build_passport(),
+    Token = cds_client:put_identity_doc(Doc, root_url(C)),
+    true = erlang:is_binary(Token),
+    ok = cds_ct_utils:store([{token, Token}, {doc, Doc}], C).
+
+-spec put_insurance_cert(config()) -> any() | no_return().
+put_insurance_cert(C) ->
+    Doc = build_insurance_cert(),
+    Token = cds_client:put_identity_doc(Doc, root_url(C)),
+    true = erlang:is_binary(Token),
+    ok = cds_ct_utils:store([{token, Token}, {doc, Doc}], C).
+
+-spec assert_doc_stored(config()) -> any() | no_return().
+assert_doc_stored(C) ->
+    Token = cds_ct_utils:lookup(token, C),
+    Doc = cds_ct_utils:lookup(doc, C),
+    Doc = cds_client:get_identity_doc(Token, root_url(C)).
+
+%%
+%% Internals
+%%
+
+root_url(C) ->
+    config(root_url, C).
+
+config(Key, Config) ->
+    config(Key, Config, undefined).
+
+config(Key, Config, Default) ->
+    case lists:keysearch(Key, 1, Config) of
+        {value, {Key, Val}} ->
+            Val;
+        _ ->
+            Default
+    end.
+
+build_passport() ->
+    {russian_domestic_passport, #ident_doc_store_RussianDomesticPassport{
+        series = <<"">>,
+        number = <<"">>,
+        issuer = <<"">>,
+        issuer_code = <<"">>,
+        issued_at = <<"">>,
+        family_name = <<"">>,
+        first_name = <<"">>,
+        patronymic = <<"">>,
+        birth_date = <<"">>,
+        birth_place = <<"">>
+    }}.
+
+build_insurance_cert() ->
+    {russian_retiree_insurance_certificate, #ident_doc_store_RussianRetireeInsuranceCertificate{
+        number = <<"">>
+    }}.
