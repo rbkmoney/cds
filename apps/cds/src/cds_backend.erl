043601f604dc37a3690d1d4491a3efedc59d26bb
WWW_cool <WWW_cool@inbox.ru>
2019-03-05 11:26:39 +0300

    map errors

--- apps/cds/src/cds_backend.erl
+++ apps/cds/src/cds_backend.erl
@@ -24,5 +24,14 @@ call(Key, Method, Args) ->
             "~p (~p) ~p failed~nStacktrace:~s",
             [Key, Module, Method,
                 lager:pr_stacktrace(erlang:get_stacktrace(), {Class, Reason})]),
-        exit(backend_error)
+        handle_error(Class, Reason)
     end.
+
+handle_error(throw, {noproc, Reason}) ->
+    BinaryDescription = erlang:list_to_binary(io_lib:format("~9999p", [Reason])),
+    woody_error:raise(system, {internal, resource_unavailable, BinaryDescription});
+handle_error(error, timeout = Reason) ->
+    BinaryDescription = erlang:list_to_binary(io_lib:format("~9999p", [Reason])),
+    woody_error:raise(system, {internal, result_unknown, BinaryDescription});
+handle_error(Class, Reason) ->
+    exit({backend_error, {Class, Reason}}).
