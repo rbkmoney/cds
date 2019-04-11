12386c82b5ecc9f819e407c0f39079146ec35808
Sergei Shuvatov <s.shuvatov@rbkmoney.com>
2019-04-11 20:16:45 +0300

    CDS-83: add PutCard and PutSession methods

--- apps/cds/src/cds_card_client.erl
+++ apps/cds/src/cds_card_client.erl
@@ -5,6 +5,8 @@
 -export([put_card_data/2]).
 -export([put_card_data/3]).
 -export([get_session_data/2]).
+-export([put_card/2]).
+-export([put_session/3]).
 
 %%
 %% Internal types
@@ -37,3 +39,11 @@ put_card_data(CardData, SessionData, RootUrl) ->
 -spec get_session_data(cds:session(), woody:url()) -> result().
 get_session_data(Session, RootUrl) ->
     cds_woody_client:call(card, 'GetSessionData', [Session], RootUrl).
+
+-spec put_card(card_data(), woody:url()) -> result().
+put_card(CardData, RootUrl) ->
+    cds_woody_client:call(card, 'PutCard', [CardData], RootUrl).
+
+-spec put_session(cds:session(), session_data(), woody:url()) -> result().
+put_session(SessionID, SessionData, RootUrl) ->
+    cds_woody_client:call(card, 'PutSession', [SessionID, SessionData], RootUrl).
