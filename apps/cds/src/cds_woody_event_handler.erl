bc02f4a0172cee262a3c28ad7c8d4a3d5587e9bf
Rustem Shaydullin <ndiezel0@gmail.com>
2019-07-14 21:51:23 +0300

    CDS-92: Port changes from kds

--- apps/cds/src/cds_woody_event_handler.erl
+++ apps/cds/src/cds_woody_event_handler.erl
@@ -3,6 +3,9 @@
 -behaviour(woody_event_handler).
 
 -include_lib("cds_proto/include/cds_proto_keyring_thrift.hrl").
+-include_lib("cds_proto/include/cds_proto_storage_thrift.hrl").
+-include_lib("dmsl/include/dmsl_cds_thrift.hrl").
+-include_lib("tds_proto/include/tds_proto_storage_thrift.hrl").
 
 %% woody_event_handler behaviour callbacks
 -export([handle_event/4]).
@@ -32,34 +35,43 @@ filter_meta(RawMeta) ->
             RawMeta
     end.
 
-filter_result(Result) ->
-    case Result of
-        [#'EncryptedMasterKeyShare'{} | _Rest] = EncryptedMasterKeyShares ->
-            filter_encrypted_master_key_shares(EncryptedMasterKeyShares);
-        {ok, [#'EncryptedMasterKeyShare'{} | _Rest] = EncryptedMasterKeyShares} ->
-            filter_encrypted_master_key_shares(EncryptedMasterKeyShares);
-        #'Keyring'{keys = Keys} = Keyring ->
-            Keyring#'Keyring'{keys = filter_keys(Keys)};
-        {ok, #'Keyring'{keys = Keys} = Keyring} ->
-            Keyring#'Keyring'{keys = filter_keys(Keys)};
-        _ ->
-            Result
-    end.
+filter_result({ok, Result}) -> {ok, filter(Result)};
+filter_result({exception, Exception}) -> {exception, filter(Exception)};
+filter_result(Result) -> filter(Result).
 
-filter_args(Args) ->
-    case Args of
-        [#'SignedMasterKeyShare'{} = SignedShare] ->
-            [SignedShare#'SignedMasterKeyShare'{signed_share = <<"***">>}];
-        _ ->
-            Args
-    end.
+filter_args(Args) -> filter(Args).
 
-filter_encrypted_master_key_shares(EncryptedMasterKeyShares) ->
-    lists:map(
-        fun (EncryptedMasterKeyShare) ->
-            EncryptedMasterKeyShare#'EncryptedMasterKeyShare'{encrypted_share = <<"***">>}
-        end,
-        EncryptedMasterKeyShares).
+filter(L) when is_list(L) -> [filter(E) || E <- L];
+filter(M) when is_map(M) -> maps:map(fun (_K, V) -> filter(V) end, M);
 
-filter_keys(Keys) ->
-    maps:map(fun (_K, #'Key'{} = Key) -> Key#'Key'{data = <<"***">>} end, Keys).
+filter(#'EncryptedMasterKeyShare'{} = EncryptedMasterKeyShare) ->
+    EncryptedMasterKeyShare#'EncryptedMasterKeyShare'{encrypted_share = <<"***">>};
+filter(#'SignedMasterKeyShare'{} = SignedShare) ->
+    SignedShare#'SignedMasterKeyShare'{signed_share = <<"***">>};
+filter(#'Keyring'{keys = Keys} = Keyring) ->
+    Keyring#'Keyring'{keys = filter(Keys)};
+filter(#'Key'{} = Key) ->
+    Key#'Key'{data = <<"***">>};
+
+filter(V) when is_integer(V) -> V;
+filter(ok) -> ok;
+filter({success, #'Success'{}} = V) -> V;
+filter({more_keys_needed, D} = V) when is_integer(D) -> V;
+filter(#'KeyringState'{} = V) -> V;
+filter(#'KeyringMeta'{} = V) -> V;
+filter(#'KeyringMetaDiff'{} = V) -> V;
+
+%% cds storage
+
+filter(#'CardData'{} = V) -> V;
+
+%% tds
+filter(#'tds_Token'{} = V) -> V;
+filter(V) when is_bitstring(V) -> V;
+
+filter(#'InvalidStatus'{} = V) -> V;
+filter(#'InvalidActivity'{} = V) -> V;
+filter(#'InvalidKeyringMeta'{} = V) -> V;
+filter(#'InvalidArguments'{} = V) -> V;
+filter(#'VerificationFailed'{} = V) -> V;
+filter(#'OperationAborted'{} = V) -> V.
