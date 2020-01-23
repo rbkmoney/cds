6ec29f8da243bca54832d4d7377e949b6874de65
Sergey Yelin <elinsn@gmail.com>
2020-01-23 19:30:59 +0300

    Revert

--- apps/cds/src/cds_utils.erl
+++ apps/cds/src/cds_utils.erl
@@ -91,9 +91,9 @@ encrypt_payload(Token, EncodedPayload) ->
     <<AAD:4/binary, _/binary>> = Token,
     {KeyID, Key} = cds_keyring:get_current_key(),
     Cipher = cds_crypto:encrypt(Key, EncodedPayload, Token, AAD),
-    <<KeyID, Cipher/binary>>.
+    <<KeyID:4/integer-unit:8, Cipher/binary>>.
 
-decrypt_payload(<<KeyID, Cipher/binary>>) ->
+decrypt_payload(<<KeyID:4/integer-unit:8, Cipher/binary>>) ->
     {ok, {KeyID, Key}} = cds_keyring:get_key(KeyID),
     cds_crypto:decrypt(Key, Cipher).
 
